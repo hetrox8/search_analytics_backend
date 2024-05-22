9e93d641320e06bae32e1f33d70c5e963e04b2c9
suleiman gacheru <dretrevor8@gmail.com>
2024-05-22 11:01:44 +0300

    refactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  root 'search_queries#index'
+  # API routes
   resources :search_queries, only: [:create, :index]
+
+  # Catch-all route to serve React frontend
+  root to: 'public#index'
+  get '*path', to: 'public#index', constraints: ->(req) { !req.xhr? && req.format.html? }
 end
